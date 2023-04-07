51ff99caf0149242d7f04a3320797e7770179f8c
20hvt02 <20hvt@JAMES>
2023-04-07 17:12:48 +0700

    .

---
+++ Androi/KiemTra/app/src/main/java/com/example/kiemtra/listViewItem.java
@@ -0,0 +1,14 @@
+package com.example.kiemtra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listViewItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view_item);
+    }
+}
---
+++ Androi/StartServices/app/src/main/java/com/example/startservices/MyService.java
@@ -0,0 +1,2 @@
+package com.example.startservices;public class MyService {
+}
